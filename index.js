f7a96807207e7595ad6b444b8d945af888116282
Rajeev Kumar <rajeevjewar2@gmail.com>
2024-01-25 13:39:10 +0530

    Node-Farm is now Ready

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+// Own created module.
+const replaceTemplate = require('./modules/replaceTemplate');
+
+// Third Party module.
+// const slugify = require('slugify');
 
 //////// //////////////////////////////////
 // FILES
@@ -33,27 +39,59 @@ console.log("Will read file!");
 
 //////// //////////////////////////////////
 // SERVER
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
-  } else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
-  } else if (pathName === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
+  const { query, pathname } = url.parse(req.url, true);
+  // Overview Page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Product Page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API Page
+  } else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
     res.end(data);
+
+    // Not Found Page
   } else {
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hello-world",
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
-    res.end("<h1>Page not Found!<h1>");
+    res.end('<h1>Page not Found!<h1>');
   }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to request on the port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on the port 8000');
 });
+
+// Just to use third party module.
+// const slug = dataObj.map((el) => slugify(el.productName, { lower: true }));
+// console.log(slug);
